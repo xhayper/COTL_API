bc1bd96575909d7c9f69c37afb7c9f19552ef515
xhayper <hayper1919@gmail.com>
2022-08-19 21:30:24 +0700

    feat: implemented InventoryItemInfo

--- COTL_API/Inventory/InventoryItemInfo.cs
+++ COTL_API/Inventory/InventoryItemInfo.cs
@@ -3,4 +3,26 @@ namespace COTL_API.InventoryItem;
 public class InventoryItemInfo
 {
 
+    public static int ItemCategory;
+    public static int SeedType;
+
+    public static string Name = LocalizedName;
+    public static string Lore = LocalizedDescription;
+    public static string Description;
+
+    public static string LocalizedName;
+    public static string LocalizedLore;
+    public static string LocalizedDescription;
+
+    public static int FuelWeight;
+    public static int FoodSatitation;
+
+    public static bool IsFish;
+    public static bool IsFood;
+    public static bool IsBigFish;
+
+    public static bool CanBeGivenToFollower;
+
+    public static string CapacityString(int amount, int minimum) { return ""; }
+
 }
