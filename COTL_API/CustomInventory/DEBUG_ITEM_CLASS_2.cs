dd51227f1906dea7a007be613c8d2dbb4205a6b7
xhayper <46065349+xhayper@users.noreply.github.com>
2022-08-20 20:15:13 +0700

    feat: added debug item

---
+++ COTL_API/CustomInventory/DEBUG_ITEM_CLASS_2.cs
@@ -0,0 +1,7 @@
+namespace COTL_API.CustomInventory;
+
+public class DEBUG_ITEM_CLASS_2 : CustomInventoryItem
+{
+    public override string LocalizedName() { return "DEBUG_ITEM_2"; }
+    public override string LocalizedDescription() { return "COTL_API'S 2ND DEBUG ITEM"; }
+}
