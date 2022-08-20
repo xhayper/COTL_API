2a9572885191ecc063f505f31e034f9431ce4fb9
xhayper <46065349+xhayper@users.noreply.github.com>
2022-08-20 20:13:57 +0700

    feat: fix debug item

--- COTL_API/CustomInventory/DEBUG_ITEM_CLASS.cs
+++ COTL_API/CustomInventory/DEBUG_ITEM_CLASS.cs
@@ -2,6 +2,6 @@ namespace COTL_API.CustomInventory;
 
 public class DEBUG_ITEM_CLASS : CustomInventoryItem
 {
-    public override string Name() { return "DEBUG_ITEM"; }
-    public override string Description() { return "COTL_API'S DEBUG ITEM"; }
+    public override string LocalizedName() { return "DEBUG_ITEM"; }
+    public override string LocalizedDescription() { return "COTL_API'S DEBUG ITEM"; }
 }
