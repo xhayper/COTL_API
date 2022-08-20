dba547adacfbe0050245d063130a50dc3179f277
xhayper <46065349+xhayper@users.noreply.github.com>
2022-08-20 19:28:10 +0700

    feat: Test

--- COTL_API/CustomInventory/CustomInventoryItem.cs
+++ COTL_API/CustomInventory/CustomInventoryItem.cs
@@ -1,20 +1,25 @@
-using System.Reflection;
-using COTL_API.Guid;
-
+using I2.Loc;
 namespace COTL_API.CustomInventory;
 
 public class CustomInventoryItem
 {
     public InventoryItem.ITEM_CATEGORIES ItemCategory;
+    public InventoryItem.ITEM_TYPE ItemType;
     public InventoryItem.ITEM_TYPE SeedType;
 
-    public string Name() { return LocalizedName(); }
-    public string Lore() { return LocalizedDescription(); }
-    public string Description() { return ""; }
+    public virtual string Name() { return LocalizedName(); }
+    public virtual string Lore() { return LocalizedLore(); }
+    public virtual string Description() { return LocalizedDescription(); }
 
-    public string LocalizedName() { return ""; }
-    public string LocalizedLore() { return ""; }
-    public string LocalizedDescription() { return ""; }
+/*
+			this._itemHeader.text = LocalizationManager.GetTranslation(string.Format("Inventory/{0}", config), true, 0, true, false, null, null, true);
+			this._itemLore.text = LocalizationManager.GetTranslation(string.Format("Inventory/{0}/Lore", config), true, 0, true, false, null, null, true);
+			this._itemDescription.text = LocalizationManager.GetTranslation(string.Format("Inventory/{0}/Description", config), true, 0, true, false, null, null, true);
+*/
+
+    public virtual string LocalizedName() { return LocalizationManager.GetTranslation(string.Format("Inventory/{0}", ItemType), true, 0, true, false, null, null, true); }
+    public virtual string LocalizedLore() { return LocalizationManager.GetTranslation(string.Format("Inventory/{0}/Lore", ItemType), true, 0, true, false, null, null, true); }
+    public virtual string LocalizedDescription() { return LocalizationManager.GetTranslation(string.Format("Inventory/{0}/Description", ItemType), true, 0, true, false, null, null, true); }
 
     public int FuelWeight;
     public int FoodSatitation;
