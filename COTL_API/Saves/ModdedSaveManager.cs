c3e7683938dd0b6ebbbc80af1c31c88bf98aee2d
xhayper <46065349+xhayper@users.noreply.github.com>
2022-08-20 07:06:54 +0700

    docs: updated README

--- COTL_API/Saves/ModdedSaveManager.cs
+++ COTL_API/Saves/ModdedSaveManager.cs
@@ -19,6 +19,7 @@ public static class ModdedSaveManager
         {
             SaveData = saveData;
         });
+
         COTLDataReadWriter<ModdedSaveData> saveFileReadWriter2 = _saveDataReadWriter;
         saveFileReadWriter2.OnCreateDefault = (Action)Delegate.Combine(saveFileReadWriter2.OnCreateDefault, (Action)delegate
         {
@@ -39,21 +40,15 @@ public static class ModdedSaveManager
     [HarmonyPostfix]
     public static void Save()
     {
-        if (DataManager.Instance.AllowSaving && !CheatConsole.IN_DEMO)
-        {
-            _saveDataReadWriter.Write(SaveData, MakeSaveSlot(SAVE_SLOT));
-        }
+        _saveDataReadWriter.Write(SaveData, MakeSaveSlot(SAVE_SLOT));
     }
 
     [HarmonyPatch(typeof(SaveAndLoad), "Load")]
     [HarmonyPostfix]
     public static void Load(int saveSlot)
     {
-        if (!CheatConsole.IN_DEMO)
-        {
-            SAVE_SLOT = saveSlot;
-            _saveDataReadWriter.Read(MakeSaveSlot(SAVE_SLOT));
-        }
+        SAVE_SLOT = saveSlot;
+        _saveDataReadWriter.Read(MakeSaveSlot(SAVE_SLOT));
     }
 
     [HarmonyPatch(typeof(SaveAndLoad), "DeleteSaveSlot")]
--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 <!-- This plugin is a BepInEx plugin made for Inscryption as an API. This is the de-facto standard API for Inscryption modders. -->
 A BepInEx plugin made for modder to easily interact with [`Cult of the Lamb`](https://store.steampowered.com/app/1313140/Cult_of_the_Lamb/) game
+
+## Credits
+
+[InscryptionAPI](https://github.com/InscryptionModding/InscryptionAPI) - Most of the code
