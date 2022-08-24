284d46d53f4ce5207f0529b64d422bdec203ce1d
Ingo Heijnens <ingohhacks@gmail.com>
2022-08-24 17:55:28 +0200

    Added categories

--- COTL_API/CustomFollowerCommand/CustomFollowerCommandManager.cs
+++ COTL_API/CustomFollowerCommand/CustomFollowerCommandManager.cs
@@ -1,4 +1,5 @@
-﻿using COTL_API.Guid;
+﻿using COTL_API.CustomInventory;
+using COTL_API.Guid;
 using HarmonyLib;
 using Lamb.UI.FollowerInteractionWheel;
 using System;
@@ -30,7 +31,42 @@ public class CustomFollowerCommandManager
     [HarmonyPostfix]
     public static void FollowerCommandGroups_DefaultCommands(Follower follower, List<CommandItem> __result)
     {
-        customCommands.Values.Do(c => { if (c.CheckSelectionPreconditions(follower)) __result.Add(c); });
+        customCommands.Values.Do(c => { if (c.GetCategories().Contains(FollowerCommandCategory.DEFAULT_COMMAND) && c.CheckSelectionPreconditions(follower)) __result.Add(c); });
+    }
+
+    [HarmonyPatch(typeof(FollowerCommandGroups), "GiveWorkerCommands", new Type[] { typeof(Follower) })]
+    [HarmonyPostfix]
+    public static void FollowerCommandGroups_GiveWorkerCommands(Follower follower, List<CommandItem> __result)
+    {
+        customCommands.Values.Do(c => { if (c.GetCategories().Contains(FollowerCommandCategory.GIVE_WORKER_COMMAND) && c.CheckSelectionPreconditions(follower)) __result.Add(c); });
+    }
+
+    [HarmonyPatch(typeof(FollowerCommandGroups), "MakeDemandCommands", new Type[] { typeof(Follower) })]
+    [HarmonyPostfix]
+    public static void FollowerCommandGroups_MakeDemandCommands(Follower follower, List<CommandItem> __result)
+    {
+        customCommands.Values.Do(c => { if (c.GetCategories().Contains(FollowerCommandCategory.MAKE_DEMAND_COMMAND) && c.CheckSelectionPreconditions(follower)) __result.Add(c); });
+    }
+
+    [HarmonyPatch(typeof(FollowerCommandGroups), "WakeUpCommands", new Type[] { })]
+    [HarmonyPostfix]
+    public static void FollowerCommandGroups_WakeUpCommands(List<CommandItem> __result)
+    {
+        customCommands.Values.Do(c => { if (c.GetCategories().Contains(FollowerCommandCategory.WAKE_UP_COMMAND)) __result.Add(c); });
+    }
+
+    [HarmonyPatch(typeof(FollowerCommandGroups), "OldAgeCommands", new Type[] { typeof(Follower) })]
+    [HarmonyPostfix]
+    public static void FollowerCommandGroups_OldAgeCommands(Follower follower, List<CommandItem> __result)
+    {
+        customCommands.Values.Do(c => { if (c.GetCategories().Contains(FollowerCommandCategory.OLD_AGE_COMMAND) && c.CheckSelectionPreconditions(follower)) __result.Add(c); });
+    }
+
+    [HarmonyPatch(typeof(FollowerCommandGroups), "DissenterCommands", new Type[] { typeof(Follower) })]
+    [HarmonyPostfix]
+    public static void FollowerCommandGroups_DissenterCommands(Follower follower, List<CommandItem> __result)
+    {
+        customCommands.Values.Do(c => { if (c.GetCategories().Contains(FollowerCommandCategory.DISSENTER_COMMAND) && c.CheckSelectionPreconditions(follower)) __result.Add(c); });
     }
 
     [HarmonyPatch(typeof(interaction_FollowerInteraction), "OnFollowerCommandFinalized", new Type[] { typeof(FollowerCommands[]) })]
