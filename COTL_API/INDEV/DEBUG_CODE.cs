1faa94ca20ccdf3caa158db9015e58739d3ae14f
xhayper <hayper1919@gmail.com>
2022-08-26 15:21:54 +0700

    chore: code cleanup

--- COTL_API/INDEV/DEBUG_CODE.cs
+++ COTL_API/INDEV/DEBUG_CODE.cs
@@ -1,32 +1,33 @@
-﻿using COTL_API.Skins;
-using HarmonyLib;
-using System;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.Collections.Generic;
+using COTL_API.Helpers;
+using COTL_API.Skins;
 using System.Text;
 using UnityEngine;
+using HarmonyLib;
+using System.IO;
+using System;
 
-namespace COTL_API.INDEV
+namespace COTL_API.INDEV;
+
+[HarmonyPatch]
+public class DEBUG_CODE
 {
-    public class DEBUG_CODE
+    public static void CreateSkin()
     {
-        public static void CreateSkin()
-        {
-            var customTex = new Texture2D(2, 2, TextureFormat.RGBA32, false);
-            byte[] imgBytes = File.ReadAllBytes(Plugin.PLUGIN_PATH + "/APIAssets/placeholder_sheet.png");
-            customTex.LoadImage(imgBytes);
-            var atlasText = File.ReadAllText(Plugin.PLUGIN_PATH + "/APIAssets/basic_atlas.txt");
+        var customTex = new Texture2D(2, 2, TextureFormat.RGBA32, false);
+        byte[] imgBytes = File.ReadAllBytes(PluginPaths.ResolveAssetPath("placeholder_sheet.png"));
+        customTex.LoadImage(imgBytes);
+        var atlasText = File.ReadAllText(PluginPaths.ResolveAssetPath("basic_atlas.txt"));
 
-            SkinManager.AddCustomSkin("Test", customTex, atlasText);
-        }
+        SkinManager.AddCustomSkin("Test", customTex, atlasText);
+    }
 
-        [HarmonyPatch(typeof(Lamb.UI.InventoryMenu), "OnShowStarted")]
-        [HarmonyPrefix]
-        public static void _____(Lamb.UI.InventoryMenu __instance)
-        {
-            Inventory.AddItem(Plugin.DEBUG_ITEM, 1, true);
-            Inventory.AddItem(Plugin.DEBUG_ITEM_2, 1, true);
-            Inventory.AddItem(Plugin.DEBUG_ITEM_3, 1, true);
-        }
+    [HarmonyPatch(typeof(Lamb.UI.InventoryMenu), "OnShowStarted")]
+    [HarmonyPrefix]
+    public static void _____(Lamb.UI.InventoryMenu __instance)
+    {
+        Inventory.AddItem(Plugin.DEBUG_ITEM, 1, true);
+        Inventory.AddItem(Plugin.DEBUG_ITEM_2, 1, true);
+        Inventory.AddItem(Plugin.DEBUG_ITEM_3, 1, true);
     }
 }
