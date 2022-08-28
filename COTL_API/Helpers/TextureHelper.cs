f1d5f9cbc88157e4bc65e523453f18d6e99c1314
Ingo Heijnens <ingohhacks@gmail.com>
2022-08-28 15:00:46 +0200

    Added texture caching

--- COTL_API/Helpers/TextureHelper.cs
+++ COTL_API/Helpers/TextureHelper.cs
@@ -1,22 +1,31 @@
 using UnityEngine;
 using System.IO;
+using System.Collections.Generic;
 
 namespace COTL_API.Helpers;
 
 public class TextureHelper
 {
+    public static Dictionary<string, Sprite> spriteCache = new();
+    public static Dictionary<string, Texture2D> textureCache = new();
+
     public static Texture2D CreateTextureFromPath(string path, TextureFormat textureFormat = TextureFormat.RGBA32,
         bool mipmaps = false, bool linear = false)
     {
+        if (textureCache.ContainsKey(path)) return textureCache[path];
         Texture2D tex = new(1, 1, textureFormat, mipmaps, linear);
         tex.filterMode = FilterMode.Point;
         tex.LoadImage(File.ReadAllBytes(path));
+        textureCache[path] = tex;
         return tex;
     }
 
     public static Sprite CreateSpriteFromPath(string path)
     {
+        if (spriteCache.ContainsKey(path)) return spriteCache[path];
         Texture2D tex = CreateTextureFromPath(path);
-        return Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), new Vector2(tex.width / 2, tex.height / 2));
+        Sprite sprite = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), new Vector2(tex.width / 2, tex.height / 2));
+        spriteCache[path] = sprite;
+        return sprite;
     }
 }
