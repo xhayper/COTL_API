9b865e29ac4ab6a57c13963fa3335ff292dbfa08
xhayper <hayper1919@gmail.com>
2022-08-28 20:37:23 +0700

    fix: typo

--- COTL_API/Debug/DebugTarotCard.cs
+++ COTL_API/Debug/DebugTarotCard.cs
@@ -5,7 +5,7 @@ namespace COTL_API.Debug;
 public class DebugTarotCard : CustomTarotCard.CustomTarotCard
 {
     public override string InternalName => "DEBUG_TAROT_CARD";
-    public override string LocalisedName(int _) { return "cOTL API'S DEBUG TAROT CARD"; }
+    public override string LocalisedName(int _) { return "COTL API'S DEBUG TAROT CARD"; }
     public override string LocalisedLore() { return "<color=\"red\">GOD LIKE!</color>"; }
     public override string LocalisedDescription(int _) { return "<sprite name=\"icon_BlueHeart\"> +25, 25x walk speed"; }
 
