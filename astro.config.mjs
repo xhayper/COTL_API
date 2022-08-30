a351a1cf636430f67b733877d9cbe48f40663856
xhayper <46065349+xhayper@users.noreply.github.com>
2022-08-30 22:06:41 +0700

    feat: initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from 'astro/config';
+import preact from '@astrojs/preact';
+import react from '@astrojs/react';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		// Enable Preact to support Preact JSX components.
+		preact(),
+		// Enable React for the Algolia search component.
+		react(),
+	],
+	site: 'https://xhayper.github.io',
+	base: '/COTL_API',
+});
