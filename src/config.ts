77bf89000b83b2a2615d947be433726544052eb8
xhayper <46065349+xhayper@users.noreply.github.com>
2022-08-30 23:08:46 +0700

    feat: aw.

--- src/config.ts
+++ src/config.ts
@@ -1,21 +1,21 @@
 export const SITE = {
-  title: "COLT_API's Documentation",
-  description: "Cult of the Lamb API's Documentation",
-  defaultLanguage: "en_US",
+    title: "COLT_API's Documentation",
+    description: "Cult of the Lamb API's Documentation",
+    defaultLanguage: "en_US"
 };
 
 export const OPEN_GRAPH = {
-  image: {
-    src: "https://github.com/withastro/astro/blob/main/assets/social/banner.jpg?raw=true",
-    alt:
-      "astro logo on a starry expanse of space," +
-      " with a purple saturn-like planet floating in the right foreground",
-  },
-  twitter: "hayper1919",
+    image: {
+        src: "https://github.com/withastro/astro/blob/main/assets/social/banner-minimal.png?raw=true",
+        alt:
+            "astro logo on a starry expanse of space," +
+            " with a purple saturn-like planet floating in the right foreground"
+    },
+    twitter: "hayper1919"
 };
 
 export const KNOWN_LANGUAGES = {
-  English: "en",
+    English: "en"
 };
 
 // Uncomment this to add an "Edit this page" button to every page of documentation.
@@ -33,14 +33,14 @@ export const COMMUNITY_INVITE_URL = `https://discord.gg/jZ2DytX3TX`;
 // }
 
 export const SIDEBAR = {
-  en: [
-    { text: "", header: true },
-    { text: "Section Header", header: true },
-    { text: "Introduction", link: "COTL_API/en/introduction" },
-    { text: "Page 2", link: "COTL_API/en/page-2" },
-    { text: "Page 3", link: "COTL_API/en/page-3" },
+    en: [
+        { text: "", header: true },
+        { text: "Section Header", header: true },
+        { text: "Introduction", link: "COTL_API/en/introduction" },
+        { text: "Page 2", link: "COTL_API/en/page-2" },
+        { text: "Page 3", link: "COTL_API/en/page-3" },
 
-    { text: "Another Section", header: true },
-    { text: "Page 4", link: "COTL_API/en/page-4" },
-  ],
+        { text: "Another Section", header: true },
+        { text: "Page 4", link: "COTL_API/en/page-4" }
+    ]
 };
