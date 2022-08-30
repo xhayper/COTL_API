f15ab521ba003f09f9a294f21cc0fc8d93cd5310
xhayper <hayper1919@gmail.com>
2022-08-30 08:02:52 +0700

    feat: experimental UI

---
+++ COTL_API/UI/Settings/SkinSettings.cs
@@ -0,0 +1,50 @@
+﻿using src.UINavigator;
+using src.Extensions;
+using UnityEngine;
+using Lamb.UI;
+using Rewired;
+using System;
+using COTL_API.Saves;
+
+namespace COTL_API.UI.Settings;
+
+
+public class SkinSettings : UISubmenuBase
+{
+
+    [SerializeField]
+    private MMHorizontalSelector _skinName;
+
+    public override void Awake()
+    {
+        base.Awake();
+        name = "Skins";
+
+        _skinName = new();
+        _skinName.name = "Selected Skin";
+
+        _skinName.PrefillContent("Default");
+        //_skinName._text.text = "Selected Skin";
+    }
+
+    private void Start()
+    {
+        _skinName.OnSelectionChanged += OnSkinValueChanged;
+    }
+
+    public void Reset()
+    {
+        APIDataManager.apiData.SetValue<string>("selectedSkin", null);
+    }
+
+    public void Configure()
+    {
+        //_skinName.ContentIndex = _skinName.Content.IndexOf(APIDataManager.apiData.GetValueAsString("selectedSkin"));
+    }
+
+    private void OnSkinValueChanged(int index)
+    {
+        string skinName = _skinName.Content[index];
+        Plugin.logger.LogInfo($"Selected Skin: {skinName}");
+    }
+}
---
+++ COTL_API/UI/UIManager.cs
@@ -0,0 +1,17 @@
+﻿using COTL_API.UI.Settings;
+using HarmonyLib;
+using Lamb.UI.SettingsMenu;
+
+namespace COTL_API.UI;
+
+[HarmonyPatch]
+public class UIManager
+{
+    [HarmonyPatch(typeof(UISettingsMenuController), nameof(UISettingsMenuController.OnShowStarted))]
+    [HarmonyPrefix]
+    public static void UISettingsMenuController_OnShowStarted(UISettingsMenuController __instance)
+    {
+        //__instance.PushInstance(new SkinSettings());
+        __instance.Push(new SkinSettings());
+    }
+}
