ba9f5a6283663aafb0f113529f2e920495ec0063
xhayper <hayper1919@gmail.com>
2022-08-30 22:49:06 +0700

    feat: stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,24 @@
-import { defineConfig } from 'astro/config';
-import preact from '@astrojs/preact';
-import react from '@astrojs/react';
+import { defineConfig } from "astro/config";
+import remarkGithub from "remark-github";
+import preact from "@astrojs/preact";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		// Enable Preact to support Preact JSX components.
-		preact(),
-		// Enable React for the Algolia search component.
-		react(),
-	],
-	site: 'https://xhayper.github.io',
-	base: '/COTL_API'
+  integrations: [
+    // Enable Preact to support Preact JSX components.
+    preact(),
+    // Enable React for the Algolia search component.
+    react(),
+  ],
+  markdown: {
+    extendDefaultPlugins: true,
+    remarkPlugins: [
+      remarkGithub({
+        repository: "https://github.com/xhayper/COTL_API.git",
+      }),
+    ],
+  },
+  site: "https://xhayper.github.io",
+  base: "/COTL_API",
 });
