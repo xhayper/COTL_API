be54826d4c48b59fadeccbfd3639682c2ecdba6e
xhayper <hayper1919@gmail.com>
2022-08-31 08:34:38 +0700

    feat: added gift support

---
+++ COTL_API/Debug/DebugGiftFollowerCommand.cs
@@ -0,0 +1,17 @@
+namespace COTL_API.Debug;
+
+public class DebugGiftFollowerCommand : CustomFollowerCommand.CustomFollowerCommand
+{
+    public override string InternalName => "DEBUG_GIFT_FOLLOWER_COMMAND";
+    public override bool ShouldAppearFor(Follower follower) => false;
+
+    public override void Execute(interaction_FollowerInteraction interaction,
+        FollowerCommands finalCommand)
+    {
+        interaction.StartCoroutine(interaction.FrameDelayCallback(delegate
+        {
+            interaction.eventListener.PlayFollowerVO(interaction.generalAcknowledgeVO);
+            interaction.follower.Brain.HardSwapToTask(new FollowerTask_InstantPoop());
+        }));
+    }
+}
--- COTL_API/Debug/DebugItemClass.cs
+++ COTL_API/Debug/DebugItemClass.cs
@@ -5,11 +5,12 @@ public class DebugItemClass : CustomInventory.CustomInventoryItem
     public override string InternalName => "DEBUG_ITEM";
     public override string LocalizedName() { return "DEBUG_ITEM"; }
     public override string LocalizedDescription() { return "COTL_API'S DEBUG ITEM"; }
-    
 
     public override bool IsCurrency => true;
+    
     public override bool IsBurnableFuel => true;
-
-    public override InventoryItem.ITEM_CATEGORIES ItemCategory => InventoryItem.ITEM_CATEGORIES.LOGS;
     public override int FuelWeight => 100;
+    
+    public override bool CanBeGivenToFollower => true;
+    public override FollowerCommands GiftCommand => Plugin.DebugGiftFollowerCommand;
 }
