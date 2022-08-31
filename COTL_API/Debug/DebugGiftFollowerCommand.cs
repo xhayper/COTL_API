096080b9485621c3e8a75a0cb08e19bc63bd5dd1
xhayper <hayper1919@gmail.com>
2022-08-31 10:59:09 +0700

    feat: for the love of god, please work

--- COTL_API/Debug/DebugGiftFollowerCommand.cs
+++ COTL_API/Debug/DebugGiftFollowerCommand.cs
@@ -10,14 +10,12 @@ public class DebugGiftFollowerCommand : CustomFollowerCommand.CustomFollowerComm
     public override void Execute(interaction_FollowerInteraction interaction,
         FollowerCommands finalCommand)
     {
-        interaction.StartCoroutine(RunEnumerator(interaction, Plugin.DebugItem));
-    }
-
-    private IEnumerator RunEnumerator(interaction_FollowerInteraction interaction, InventoryItem.ITEM_TYPE type)
-    {
-        interaction.eventListener.PlayFollowerVO(interaction.positiveAcknowledgeVO);
-        interaction.follower.Brain.HardSwapToTask(new FollowerTask_InstantPoop());
-        Inventory.AddItem(type, -1);
+        interaction.StartCoroutine(interaction.FrameDelayCallback(delegate
+        {
+            interaction.eventListener.PlayFollowerVO(interaction.positiveAcknowledgeVO);
+            interaction.follower.Brain.HardSwapToTask(new FollowerTask_InstantPoop());
+            Inventory.AddItem(Plugin.DebugItem, -1);
+        }));
         interaction.Close();
     }
 }
