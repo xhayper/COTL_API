36924bdd9824a725f72eb5f09510c0da0f17b37b
xhayper <hayper1919@gmail.com>
2022-09-01 07:21:33 +0700

    feat: site-map

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,13 @@
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 import preact from "@astrojs/preact";
 import react from "@astrojs/react";
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [
-    // Enable Preact to support Preact JSX components.
-    preact(),
-    // Enable React for the Algolia search component.
-    react(),
-  ],
+  preact(),
+  react(),
+  sitemap()],
   site: "https://xhayper.github.io",
   base: "/COTL_API"
 });
