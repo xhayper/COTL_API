711d57f6fd51c95abef1d3247b0c9301e121f0ff
Ingo Heijnens <ingohhacks@gmail.com>
2022-09-16 17:38:07 +0200

    Added debug task

---
+++ COTL_API/Debug/DebugTaskFollowerCommand.cs
@@ -0,0 +1,38 @@
+﻿using COTL_API.Helpers;
+
+namespace COTL_API.Debug;
+
+public class DebugTaskFollowerCommand : CustomFollowerCommand.CustomFollowerCommand
+{
+    public override string InternalName => "DEBUG_TASK_FOLLOWER_COMMAND";
+    
+
+    public override string GetTitle(Follower follower)
+    {
+        return "Debug Task";
+    }
+
+    public override string GetDescription(Follower follower)
+    {
+        return "Do Debug Task at Debug Structure";
+    }
+
+    public override void Execute(interaction_FollowerInteraction interaction,
+        FollowerCommands finalCommand)
+    {
+        interaction.StartCoroutine(interaction.FrameDelayCallback(delegate
+        {
+            interaction.eventListener.PlayFollowerVO(interaction.generalAcknowledgeVO);
+
+            Structure str = TaskUtils.GetAvailableStructureOfType<DebugStructure>();
+            if (str == null)
+            {
+                interaction.eventListener.PlayFollowerVO(interaction.negativeAcknowledgeVO);
+                interaction.CloseAndSpeak("No_Debug_Structures");
+                return;
+            }
+            interaction.follower.Brain.HardSwapToTask(new DebugTask());
+        }));
+        interaction.Close();
+    }
+}
