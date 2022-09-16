01a970bccd4e44760b18ef1808588b34faac4162
Ingo Heijnens <ingohhacks@gmail.com>
2022-09-16 14:48:18 +0200

    Added structures and basic task functionality

---
+++ COTL_API/Tasks/CustomTask.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace COTL_API.Tasks;
+
+public class CustomTask : FollowerTask
+{
+    public virtual string InternalName { get; }
+
+    public FollowerTaskType TaskType;
+    public override FollowerTaskType Type => TaskType;
+
+    public string ModPrefix;
+    
+    public override int GetSubTaskCode()
+    {
+        return 0;
+    }
+
+    public override void TaskTick(float deltaGameTime) { }
+
+    public override Vector3 UpdateDestination(Follower follower)
+    {
+        if (follower == null)
+        {
+            return _brain.LastPosition;
+        }
+        return follower.transform.position;
+    }
+    
+    public override FollowerLocation Location => FollowerLocation.Base;
+
+}
