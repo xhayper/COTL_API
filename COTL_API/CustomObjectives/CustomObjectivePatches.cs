84aa1ff5c01ab0a45edab6c47f97a390914bb36e
p1xel8ted <10510767+p1xel8ted@users.noreply.github.com>
2022-09-21 02:42:22 +1000

    Added CustomObjectiveManager.cs.Centralised adding/tracking of objectives added by plugins via the API.

---
+++ COTL_API/CustomObjectives/CustomObjectivePatches.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+using MMTools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace COTL_API.CustomObjectives;
+
+[HarmonyPatch]
+public class CustomObjectivePatches
+{
+    [HarmonyPatch(typeof(interaction_FollowerInteraction), nameof(interaction_FollowerInteraction.GetConversationEntry), typeof(Follower.ComplaintType), typeof(ObjectivesData))]
+    [HarmonyPostfix]
+    private static void GetConversationEntry(Follower.ComplaintType ComplaintForBark, ObjectivesData objective, ref List<ConversationEntry> __result)
+    {
+        CustomObjective customObjective = CustomObjectiveManager.CustomObjectives.Values.FirstOrDefault(x => x.ObjectiveData.ID == objective.ID);
+        if (customObjective == null)
+        {
+            Plugin.Logger.LogWarning($"No matching quest found for {objective.ID}!");
+            return;
+        }
+
+        Plugin.Logger.LogWarning($"Matching quest found for {objective.ID}! ListCount: {__result.Count}");
+        __result[0].TermToSpeak = customObjective.InitialQuestText();
+    }
+}
---
+++ COTL_API/Debug/DebugObjective.cs
@@ -0,0 +1,21 @@
+﻿using COTL_API.CustomObjectives;
+using UnityEngine;
+
+namespace COTL_API.Debug;
+
+public sealed class DebugObjective: CustomObjective
+{
+
+    public override string InternalName()
+    {
+        return "DEBUG_OBJECTIVE";
+    }
+
+    public override string InitialQuestText()
+    {
+        return "This is a debug quest!";
+    }
+
+    public override ObjectivesData ObjectiveData => 
+        CustomObjectiveManager.Objective.CollectItem(this.ObjectiveKey, InventoryItem.ITEM_TYPE.Necklace_1, Random.Range(15, 26), false, FollowerLocation.Dungeon1_1, 4800f);
+}
