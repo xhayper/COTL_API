1db6ac6bad2e06a7dec0f9f710ec25492ddf4fd0
xhayper <46065349+xhayper@users.noreply.github.com>
2022-09-24 11:41:59 +0700

    feat: image optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,11 @@ import partytown from "@astrojs/partytown";
 import sitemap from "@astrojs/sitemap";
 import preact from "@astrojs/preact";
 import react from "@astrojs/react";
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), react(), sitemap(), partytown()],
+  integrations: [preact(), react(), sitemap(), partytown(), image()],
   vite: {
     build: {
       sourcemap: true
