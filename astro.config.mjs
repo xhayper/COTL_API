1ab38eb228c9ae07a3894635326f88802ae5c222
xhayper <46065349+xhayper@users.noreply.github.com>
2022-09-24 11:22:18 +0700

    feat: partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import preact from "@astrojs/preact";
 import react from "@astrojs/react";
+import partytown from "@astrojs/partytown";
 
+// https://astro.build/config
 export default defineConfig({
-    integrations: [preact(), react(), sitemap()],
-    site: "https://xhayper.github.io",
-    base: "/COTL_API"
+  integrations: [preact(), react(), sitemap(), partytown()],
+  site: "https://xhayper.github.io",
+  base: "/COTL_API"
 });
