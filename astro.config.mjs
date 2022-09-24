57d32a282c294a9d60d4451ad57134bfeadae9d2
xhayper <46065349+xhayper@users.noreply.github.com>
2022-09-24 11:07:24 +0700

    chore: dependencies update feat: 10 contributors

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,7 @@ import preact from "@astrojs/preact";
 import react from "@astrojs/react";
 
 export default defineConfig({
-  integrations: [
-  preact(),
-  react(),
-  sitemap()],
-  site: "https://xhayper.github.io",
-  base: "/COTL_API"
+    integrations: [preact(), react(), sitemap()],
+    site: "https://xhayper.github.io",
+    base: "/COTL_API"
 });
