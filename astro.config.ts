3aff7d96a828cdbd603f02e5bcdfb7f2faf9ec25
xhayper <hayper1919@gmail.com>
2022-09-28 12:07:11 +0700

    feat: migrate to vercel

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,7 @@
+import vercel from "@astrojs/vercel/serverless";
 import { defineConfig } from "astro/config";
 import partytown from "@astrojs/partytown";
-import robotsTxt from 'astro-robots-txt';
+import robotsTxt from "astro-robots-txt";
 import remarkGithub from "remark-github";
 import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
@@ -8,6 +9,8 @@ import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
+    output: "server",
+    adapter: vercel(),
     integrations: [react(), sitemap(), partytown({}), image(), robotsTxt()],
     vite: {
         build: {
@@ -25,7 +28,6 @@ export default defineConfig({
             ]
         ]
     },
-    site: "https://xhayper.github.io",
-    base: "/COTL_API",
+    site: "https://cotl-api.vercel.app",
     trailingSlash: "ignore"
 });
