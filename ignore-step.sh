61dc7b90d7760eaa94f041a4d5042bb79dcc2e9c
xhayper <46065349+xhayper@users.noreply.github.com>
2022-10-02 12:31:58 +0700

    Update ignore-step.sh

--- ignore-step.sh
+++ ignore-step.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-allowedChanges=("./astro.config.ts" "./package.json" "./src/" "./public/" "./.yarnrc.yml" "./.yarn/" "./tsconfig.json")
+allowedChanges=("../astro.config.ts" "../package.json" "../src/" "../public/" "../.yarnrc.yml" "../.yarn/" "../tsconfig.json")
 
 for i in "${allowedChanges[@]}"; do
-  flag=$(git diff HEAD^ HEAD --quiet "$i")
+  flag=$(git diff HEAD^ HEAD --quiet -- "$i")
   echo $flag
   if [ "$flag" != '' ]; then
         exit 1
