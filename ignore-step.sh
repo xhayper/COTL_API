e61695eabc6cd4db1bdc7a56aa2642f6fdca0a42
hayper <hayper1919@gmail.com>
2022-10-02 12:20:14 +0700

    Update ignore-step.sh

--- ignore-step.sh
+++ ignore-step.sh
@@ -2,7 +2,7 @@
 
 allowedChanges = ("./astro.config.ts" "./package.json" "./src/" "./public/" "./.yarnrc.yml" "./.yarn/" "./tsconfig.json")
 
-for i in allowedChanges do
+for i in ${allowedChanges[@]}; do
   flag = $(git diff HEAD^ HEAD --quiet $i)
   if [ flag == "" ] then exit 1 fi
 done
