f9e058a3e5145520fe969e2e978bc605aa2e5fc3
xhayper <hayper1919@gmail.com>
2022-10-17 00:46:43 +0700

    Revert "feat: maybe this iwll fix it?"
    This reverts commit 30bb93cf2dd7bc4bfa6ac211434a8820d52a6649.

--- COTL_API/Saves/APIData.cs
+++ COTL_API/Saves/APIData.cs
@@ -4,11 +4,11 @@ namespace COTL_API.Saves;
 
 internal class APIData
 {
-    internal Dictionary<string, dynamic> Data = new();
+    internal Dictionary<string, object> Data = new();
 
     internal T GetValue<T>(string key)
     {
-        Data ??= new Dictionary<string, dynamic>();
+        Data ??= new Dictionary<string, object>();
 
         if (!Data.ContainsKey(key))
             Data.Add(key, default(T));
@@ -49,7 +49,7 @@ internal class APIData
 
     internal void SetValue<T>(string key, T value)
     {
-        Data ??= new Dictionary<string, dynamic>();
+        Data ??= new Dictionary<string, object>();
 
         if (!Data.ContainsKey(key))
             Data.Add(key, value);
