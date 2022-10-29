5728c98913cfa94043fc203e760672ef9c20dddd
Ingo Heijnens <ingohhacks@gmail.com>
2022-10-29 13:25:28 +0200

    Support for player skins + various improvements

--- COTL_API/CustomSkins/CustomSkinManager.cs
+++ COTL_API/CustomSkins/CustomSkinManager.cs
@@ -1,24 +1,20 @@
-﻿using LeTai.Asset.TranslucentImage;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Spine.Unity;
-using System.Linq;
 using UnityEngine;
-using HarmonyLib;
-using Lamb.UI;
 using System;
 using Spine;
-using Spine.Unity.AttachmentTools;
-using UnityEngine.Experimental.Rendering;
 
 namespace COTL_API.CustomSkins;
 
-[HarmonyPatch]
-public class CustomSkinManager
+public partial class CustomSkinManager
 {
     internal static readonly Dictionary<string, SpineAtlasAsset> CustomAtlases = new();
     internal static readonly Dictionary<string, Skin> CustomSkins = new();
+    internal static readonly Dictionary<string, bool> AlwaysUnlockedSkins = new();
     internal static readonly Dictionary<string, Texture2D> SkinTextures = new();
     internal static readonly Dictionary<string, Material> SkinMaterials = new();
+
+    internal static List<Skin> PlayerSkinOverride = null;
 
     internal static readonly List<Tuple<int, string>> Slots = new() {
         Tuple.Create(38, "ARM_LEFT_SKIN"),
@@ -298,92 +294,58 @@ public class CustomSkinManager
         { "LEFT_EYE_FIRE_7", Tuple.Create(85, "Face/EYE_FIRE7") }
     };
     
-    public static void Add(CustomSkin skin)
+    public static void Add(CustomFollowerSkin followerSkin)
     {
-        string atlasText = skin.GenerateAtlasText();
-        AddCustomSkin(skin.Name, skin.Texture, atlasText, skin.Colors, skin.Hidden, skin.Unlocked, skin.TwitchPremium,
-            skin.Invariant);
+        string atlasText = followerSkin.GenerateAtlasText();
+        AddCustomSkin(followerSkin.Name, followerSkin.Texture, atlasText, followerSkin.Colors, followerSkin.Hidden, followerSkin.Unlocked, followerSkin.TwitchPremium,
+            followerSkin.Invariant);
     }
 
     public static void AddCustomSkin(string name, Texture2D sheet, string atlasText, List<WorshipperData.SlotsAndColours> colors, bool hidden = false, bool unlocked = true, bool twitchPremium = false, bool invariant = false)
     {
-        sheet.name = atlasText.Replace("\r", "").Split('\n')[1].Trim();
+        Material mat;
+        SpineAtlasAsset atlas;
+        List<Tuple<int, string, float, float, float, float>> overrides = SkinUtils.CreateSkinAtlas(name, sheet, atlasText, RegionOverrideFunction, out mat, out atlas);
         SkinTextures.Add(name, sheet);
-
-        Material mat = new(Shader.Find("Spine/Skeleton")) {
-            mainTexture = sheet
-        };
         SkinMaterials.Add(name, mat);
-
-        Material[] materials = { mat };
-        SpineAtlasAsset atlas = SpineAtlasAsset.CreateRuntimeInstance(new TextAsset(atlasText), materials, true);
         CustomAtlases.Add(name, atlas);
 
-        List<Tuple<int, string>> overrideRegions = new();
-        
-        foreach (AtlasRegion region in atlas.GetAtlas().regions)
-        {
-            string simpleName = region.name;
-            if (simpleName.Contains("#")) simpleName = simpleName.Split('#')[0];
-            
-            if (SimplifiedSkinNames.TryGetValue(simpleName, out Tuple<int, string> simplified))
-            {
-                overrideRegions.Add(simplified);
-                region.name = (SlotsEnum)simplified.Item1 + ":" + simplified.Item2;
-                continue;
-            }
-            else if (simpleName.Contains(":"))
-            {
-                try {
-                    string rName = simpleName.Split(':')[1];
-                    int regionIndex = (int)(SlotsEnum)Enum.Parse(typeof(SlotsEnum), simpleName.Split(':')[0]);
-                    overrideRegions.Add(Tuple.Create(regionIndex, rName));
-                    continue;
-                } catch (Exception e) {
-                    // ignored
-                }
-            }
-            Plugin.Logger.LogError($"Failed to parse region with name: {simpleName}");
-        }
-        
-        List<Tuple<int, string, float, float, float, float>> overrides = new();
-        List<AtlasRegion> list = atlas.GetAtlas().regions;
-        for (int index = 0; index < list.Count; index++)
-        {
-            float[] scale = new[] { 0f, 0f, 1f, 1f };
-            AtlasRegion atlasRegion = list[index];
-            string[] nameSplit = atlasRegion.name.Split('#');
-            if (nameSplit.Length == 2)
-            {
-                string scales = nameSplit[1];
-                string[] scaleSplit = scales.Split(',');
-                if (scaleSplit.Length == 2)
-                {
-                    scale[2] = float.Parse(scaleSplit[0]);
-                    scale[3] = float.Parse(scaleSplit[1]);
-                }
-                else if (scaleSplit.Length == 4)
-                {
-                    scale[0] = float.Parse(scaleSplit[2]);
-                    scale[1] = float.Parse(scaleSplit[3]);
-                    scale[2] = float.Parse(scaleSplit[0]);
-                    scale[3] = float.Parse(scaleSplit[1]);
-                }
-                else
-                {
-                    Plugin.Logger.LogWarning($"Invalid scale length: {scale.Length}");
-                }
-
-                atlasRegion.name = nameSplit[0];
-            }
-            overrides.Add(Tuple.Create(overrideRegions[index].Item1, overrideRegions[index].Item2, scale[0], scale[1], scale[2], scale[3]));
-        }
-
-        CreateNewFollowerType(name, colors, hidden, unlocked, twitchPremium, invariant);
-        CreateSkin(name, overrides);
+        CreateNewFollowerType(name, colors, hidden, twitchPremium, invariant);
+        CreateSkin(name, overrides, unlocked);
     }
 
-    internal static void CreateNewFollowerType(string name, List<WorshipperData.SlotsAndColours> colors, bool hidden = false, bool unlocked = true, bool twitchPremium = false, bool invariant = false)
+    private static Tuple<int, string> RegionOverrideFunction(AtlasRegion region)
+    {
+        string simpleName = region.name;
+        string add = "";
+        if (simpleName.Contains("#"))
+        {
+            string[] split = simpleName.Split('#');
+            add = "#" + split[1];
+            simpleName = split[0];
+        }
+            
+        if (SimplifiedSkinNames.TryGetValue(simpleName, out Tuple<int, string> simplified))
+        {
+            region.name = simplified.Item1 + ":" + simplified.Item2 + add;
+            return simplified;
+        }
+
+        if (simpleName.Contains(":"))
+        {
+            try {
+                string rName = simpleName.Split(':')[1];
+                int regionIndex = (int)(SlotsEnum)Enum.Parse(typeof(SlotsEnum), simpleName.Split(':')[0]);
+                region.name = regionIndex + ":" + rName + "#" + add;
+                return Tuple.Create(regionIndex, rName);
+            } catch (Exception e) {
+                // ignored
+            }
+        }
+        return null;
+    }
+
+    internal static void CreateNewFollowerType(string name, List<WorshipperData.SlotsAndColours> colors, bool hidden = false, bool twitchPremium = false, bool invariant = false)
     {
         WorshipperData.Instance.Characters.Add(new WorshipperData.SkinAndData() {
             Title = name,
@@ -400,230 +362,30 @@ public class CustomSkinManager
         });
     }
 
-    internal static void CreateSkin(string name, List<Tuple<int, string, float, float, float, float>> overrides)
+    internal static void CreateSkin(string name, List<Tuple<int, string, float, float, float, float>> overrides, bool unlocked)
     {
-        // Create skin
         Skin skin = new(name);
-
         Skin dog = WorshipperData.Instance.SkeletonData.Skeleton.Data.FindSkin("Dog");
-        dog.Attachments.ToList().ForEach(att =>
-        {
-            if (!overrides.Any(o => o.Item1 == att.SlotIndex && o.Item2 == att.Name))
-            {
-                skin.SetAttachment(att.SlotIndex, att.Name, att.Attachment.Copy());
-            }
-        });
-        dog.Bones.ToList().ForEach(bone =>
-        {
-            skin.Bones.Add(bone);
-        });
-        dog.Constraints.ToList().ForEach(con =>
-        {
-            skin.Constraints.Add(con);
-        });
 
-        foreach (Tuple<int, string, float, float, float, float> ovr in overrides)
-        {
-            string ovrName = ovr.Item2;
-            int slot = ovr.Item1;
-            float translationX = ovr.Item3;
-            float translationY = ovr.Item4;
-            float scaleX = ovr.Item5;
-            float scaleY = ovr.Item6;
-            AtlasRegion atlasRegion = CustomAtlases[name].GetAtlas().FindRegion((SlotsEnum)slot + ":" + ovrName);
-            Attachment a = WorshipperData.Instance.SkeletonData.Skeleton.Data.FindSkin("Dog")
-                .GetAttachment(slot, ovrName).Copy();
-
-            if (ovrName == "MARKINGS")
-            {
-                a = WorshipperData.Instance.SkeletonData.Skeleton.Data.FindSkin("Dog")
-                    .GetAttachment(78, "HEAD_SKIN_TOP").Copy();
-            }
-            
-            if (a is MeshAttachment customAttachment)
-            {
-                float minX = int.MaxValue;
-                float maxX = int.MinValue;
-                float minY = int.MaxValue;
-                float maxY = int.MinValue;
-                
-                for (int j = 0; j < customAttachment.Vertices.Length; j++)
-                {
-                    if (j % 3 == 0)
-                    {
-                        minY = Math.Min(minY, customAttachment.Vertices[j]);
-                        maxY = Math.Max(maxY, customAttachment.Vertices[j]);
-                    }
-                    else if (j % 3 == 1)
-                    {
-                        minX = Math.Min(minX, customAttachment.Vertices[j]);
-                        maxX = Math.Max(maxX, customAttachment.Vertices[j]);
-                    }
-                }
-                
-                float diffX = maxX - minX;
-                float diffY = maxY - minY;
-
-                minX += translationX;
-                minY += translationY;
-                
-                float centerX = minX + (diffX / 2.0f);
-                float centerY = minY + (diffY / 2.0f);
-                
-                minX = centerX - ((diffX / 2.0f) * scaleX);
-                maxX = centerX + ((diffX / 2.0f) * scaleX);
-                minY = centerY - ((diffY / 2.0f) * scaleY);
-                maxY = centerY + ((diffY / 2.0f) * scaleY);
-
-                customAttachment.Name = "Custom" + ovrName;
-                customAttachment.SetRegion(atlasRegion);
-                atlasRegion.name = "Custom" + atlasRegion.name;
-                customAttachment.HullLength = 4;
-                customAttachment.Triangles = new[] { 1, 2, 3, 1, 3, 0 };
-                float pw = atlasRegion.page.width;
-                float ph = atlasRegion.page.height;
-                float x = atlasRegion.x;
-                float y = atlasRegion.y;
-                float w = atlasRegion.width;
-                float h = atlasRegion.height;
-                customAttachment.UVs = new[]
-                    { (x + w) / pw, y / ph, (x + w) / pw, (y + h) / ph, x / pw, (y + h) / ph, x / pw, y / ph };
-                customAttachment.Vertices = new[] { minY, minX, 1, maxY, minX, 1, maxY, maxX, 1, minY, maxX, 1 };
-                customAttachment.WorldVerticesLength = 8;
-
-                skin.SetAttachment(slot, ovrName, customAttachment);
-            }
-            else
-                Plugin.Logger.LogWarning(ovr + " is not a MeshAttachment. Skipping.");
-        }
-        
-        
-        Material runtimeMaterial;
-        Texture2D runtimeTexture;
-        
-        var skin2 = skin.GetRepackedSkin(name, SkinMaterials[name], out runtimeMaterial, out runtimeTexture);
-        _cachedTextures.Clear();
-        AtlasUtilities.ClearCache();
-        
-        foreach (Tuple<int, string, float, float, float, float> ovr in overrides)
-        {
-            string ovrName = ovr.Item2;
-            int slot = ovr.Item1;
-            Attachment a = skin2.GetAttachment(slot, ovrName).Copy();
-            if (a is MeshAttachment mesh)
-            {
-                AtlasRegion atlasRegion = mesh.RendererObject as AtlasRegion;
-                if (atlasRegion != null)
-                {
-                    float pw = atlasRegion.page.width;
-                    float ph = atlasRegion.page.height;
-                    float x = atlasRegion.x;
-                    float y = atlasRegion.y;
-                    float w = atlasRegion.width;
-                    float h = atlasRegion.height;
-                    mesh.Triangles = new[] { 1, 2, 3, 1, 3, 0 };
-                    mesh.UVs = new[]
-                        { (x + w) / pw, 1-((y + h) / ph), (x + w) / pw, 1-(y / ph), x / pw, 1-(y / ph), x / pw, 1-((y + h) / ph) };
-                    mesh.WorldVerticesLength = 8;
-                }
-
-                skin2.SetAttachment(slot, ovrName, mesh);
-            }
-        }
+        Skin skin2 = SkinUtils.ApplyAllOverrides(dog, skin, overrides, SkinMaterials[name], CustomAtlases[name]);
         
         CustomSkins.Add(name, skin2);
-        
+        AlwaysUnlockedSkins.Add(name, unlocked);
     }
 
-    [HarmonyPatch(typeof(SkeletonData), nameof(SkeletonData.FindSkin), new[] { typeof(string) })]
-    [HarmonyPostfix]
-    public static void SkinPatch(ref Skin __result, SkeletonData __instance, string skinName)
+    public static void SetPlayerSkinOverride(Skin normalSkin, Skin hurtSkin = null, Skin hurtSkin2 = null)
     {
-        if (__result != null) return;
-        if (!CustomSkins.ContainsKey(skinName)) return;
-
-        DataManager.SetFollowerSkinUnlocked(skinName);
-        __result = CustomSkins[skinName];
+        List<Skin> skins = new() {
+            normalSkin,
+            hurtSkin,
+            hurtSkin2
+        };
+        if (PlayerSkinOverride != null) Plugin.Logger.LogWarning("PlayerSkinOverride already exists. Overwriting.");
+        PlayerSkinOverride = skins;
     }
     
-    private static Dictionary<string, Texture2D> _cachedTextures = new Dictionary<string, Texture2D>();
-
-    [HarmonyPatch(typeof(Graphics), "CopyTexture", new[] { typeof(Texture), typeof(int), typeof(int), typeof(int), typeof(int), typeof(int), typeof(int), typeof(Texture), typeof(int), typeof(int), typeof(int), typeof(int) })]
-    [HarmonyPrefix]
-    public static bool Graphics_CopyTexture(ref Texture src, int srcElement, int srcMip, int srcX, int srcY, int srcWidth, int srcHeight, ref Texture dst, int dstElement, int dstMip, int dstX, int dstY)
+    public static void SetPlayerSkinOverride(CustomPlayerSkin skin)
     {
-        if (src is Texture2D s2d)
-        {
-            if (src.graphicsFormat != dst.graphicsFormat)
-            {
-                Texture2D orig;
-                if (_cachedTextures.TryGetValue(src.name, out var cached))
-                {
-                    Plugin.Logger.LogDebug($"Using cached texture {src.name} ({cached.width}x{cached.height})");
-                    orig = cached;
-                }
-                else
-                {
-                    Plugin.Logger.LogDebug($"Copying texture {src.name} ({src.width}x{src.height}) to {dst.name} ({src.width}x{src.height} with different formats: {src.graphicsFormat} to {dst.graphicsFormat}");
-                    orig = DuplicateTexture(s2d, dst.graphicsFormat);
-                    _cachedTextures[src.name] = orig;
-                }
-                Texture2D dst2d = (Texture2D)dst;
-                var fullPix = orig.GetPixels32();
-                var croppedPix = new Color32[srcWidth * srcHeight];
-                for (int i = 0; i < srcHeight; i++)
-                {
-                    for (int j = 0; j < srcWidth; j++)
-                    {
-                        croppedPix[(i * srcWidth) + j] = fullPix[((i + srcY) * orig.width) + j + srcX];
-                    }
-                }
-
-                dst2d.SetPixels32(croppedPix);
-            }
-            return false;
-        }
-
-        return true;
-    }
-    
-    private static Texture2D DuplicateTexture(Texture2D source, GraphicsFormat format)
-    {
-        RenderTexture renderTex = RenderTexture.GetTemporary(
-            source.width,
-            source.height,
-            0,
-            RenderTextureFormat.Default,
-            RenderTextureReadWrite.Linear
-        );
- 
-        Graphics.Blit(source, renderTex);
-        RenderTexture previous = RenderTexture.active;
-        RenderTexture.active = renderTex;
-        Texture2D readableText = new(source.width, source.height, format, TextureCreationFlags.None);
-        readableText.ReadPixels(new Rect(0, 0, renderTex.width, renderTex.height), 0, 0);
-        readableText.Apply();
-        RenderTexture.active = previous;
-        RenderTexture.ReleaseTemporary(renderTex);
-        return readableText;
-    }
-    
-    [HarmonyPatch(typeof(FollowerInformationBox), nameof(FollowerInformationBox.ConfigureImpl))]
-    [HarmonyPostfix]
-    public static void FollowerInformationBox_ConfigureImpl(FollowerInformationBox __instance) {
-        if (SkinTextures.ContainsKey(__instance.FollowerInfo.SkinName))
-            __instance.FollowerSpine.Skeleton.Skin = CustomSkins[__instance.FollowerInfo.SkinName];
-    }
-
-    // TODO: Temp fix. Destroy the transparent image used when recruiting follower. It hides custom meshes due to render order.
-    // Need to find out how to fix render order.
-    [HarmonyPatch(typeof(UIFollowerIndoctrinationMenuController),
-        nameof(UIFollowerIndoctrinationMenuController.OnShowStarted))]
-    [HarmonyPostfix]
-    public static void UIFollowerIndoctrinationMenuController_OnShowStarted(
-        UIFollowerIndoctrinationMenuController __instance)
-    {
-        GameObject image = __instance.gameObject.GetComponentsInChildren(typeof(TranslucentImage))[0].gameObject;
-        UnityEngine.Object.Destroy(image);
+        skin.Apply();
     }
 }
