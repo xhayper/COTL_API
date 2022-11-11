10d71a2f7a714d6f8396e4a365deba4421716a5e
KBMackenzie <109933089+KBMackenzie@users.noreply.github.com>
2022-11-11 06:57:16 -0300

    Added more documentation comments

--- COTL_API/UI/Base/UIMenuBase.cs
+++ COTL_API/UI/Base/UIMenuBase.cs
@@ -2,8 +2,14 @@
 
 namespace COTL_API.UI;
 
+/// <summary>
+/// An abstract class that serves as a base for an UI menu controller.
+/// </summary>
 public abstract class UIMenuBase : MonoBehaviour
 {
+    /// <summary>
+    /// The parent GameObject you should attach your GameObject(s) to.
+    /// </summary>
     public static Transform Parent;
 
     public void Start()
@@ -11,5 +17,10 @@ public abstract class UIMenuBase : MonoBehaviour
         InitializeMenu(Parent);
     }
 
+    /// <summary>
+    /// A method you must override to initialize your menu in.
+    /// You should attach your GameObject(s) to the <c>Transform parent</c> parameter.
+    /// </summary>
+    /// <param name="parent">The parent Transform you should attach at least one GameObject to.</param>
     public abstract void InitializeMenu(Transform parent);
 };
