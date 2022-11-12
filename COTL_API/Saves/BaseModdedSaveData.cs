d91a82b1017cd8ce20af3acb1bcb18751dccf36d
xhayper <46065349+xhayper@users.noreply.github.com>
2022-11-12 22:03:22 +0700

    chore: code cleanup

--- COTL_API/Saves/BaseModdedSaveData.cs
+++ COTL_API/Saves/BaseModdedSaveData.cs
@@ -12,12 +12,6 @@ public abstract class BaseModdedSaveData
     public abstract bool IsLoaded { get; protected set; }
     public abstract string GUID { get; protected set; }
 
-    // TODO: Make this an enum
-    /**
-     * If the save data should load when the game is open or when the save slot is loaded.
-     * true if the save data should load when the game is open.
-     * false if the save data should load when the save slot is loaded.
-     */
     public abstract ModdedSaveLoadOrder LoadOrder { get; set; }
 
     public System.Action OnSaveCompleted { get; set; }
