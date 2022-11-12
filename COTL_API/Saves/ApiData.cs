16db684588287ff2b8016f990579591a4f8eaee9
xhayper <46065349+xhayper@users.noreply.github.com>
2022-11-12 21:53:19 +0700

    chore: code smell

--- COTL_API/Saves/ApiData.cs
+++ COTL_API/Saves/ApiData.cs
@@ -2,10 +2,10 @@ namespace COTL_API.Saves;
 
 internal class ApiData
 {
-    private string[] RandomEasteregg =
-        { "Rawr~", "Hello world!", "Another day another unstable API! Hello!", "Proud bug maker!" };
+    private readonly string[] _randomEasteregg =
+        { "Rawr~", "Hewwo, twis pwoject was made bwy a fwwy, and I'm suwe you'ww wuv it!", "Another day another unstable API! Hello!", "Proud bug maker!" };
 
-    public string OwO => RandomEasteregg[UnityEngine.Random.Range(0, RandomEasteregg.Length)];
+    public string OwO => _randomEasteregg[UnityEngine.Random.Range(0, _randomEasteregg.Length)];
 
     public ObjectDictionary EnumData { get; } = new();
 }
