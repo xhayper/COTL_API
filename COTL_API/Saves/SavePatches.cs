4347073c4d3d6b47ed72e4e5b53eb08530337472
xhayper <hayper1919@gmail.com>
2022-11-12 19:25:25 +0700

    feat: experimental with save data not working

--- COTL_API/Saves/SavePatches.cs
+++ COTL_API/Saves/SavePatches.cs
@@ -5,6 +5,26 @@ namespace COTL_API.Saves;
 
 public static partial class ModdedSaveManager
 {
+    [HarmonyPatch(typeof(SaveAndLoad), nameof(SaveAndLoad.Load))]
+    [HarmonyPostfix]
+    private static void SaveAndLoad_Load(int saveSlot)
+    {
+        foreach (var saveData in _moddedSaveData.Values.Where(save => !save.LoadOnStart))
+        {
+            saveData.Load(saveSlot);
+        }
+    }
+
+    [HarmonyPatch(typeof(SaveAndLoad), nameof(SaveAndLoad.Save))]
+    [HarmonyPostfix]
+    private static void SaveAndLoad_Save()
+    {
+        foreach (var saveData in _moddedSaveData.Values)
+        {
+            saveData.Save();
+        }
+    }
+
     [HarmonyPatch(typeof(SaveAndLoad), nameof(SaveAndLoad.ResetSave))]
     [HarmonyPostfix]
     private static void SaveAndLoad_ResetSave(int saveSlot, bool newGame)
@@ -23,27 +43,5 @@ public static partial class ModdedSaveManager
         {
             saveData.DeleteSaveSlot(saveSlot);
         }
-    }
-
-    [HarmonyPatch(typeof(SaveAndLoad), MethodType.Constructor)]
-    [HarmonyPostfix]
-    private static void SaveAndLoad_Constructor(ref SaveAndLoad __instance)
-    {
-        __instance._saveFileReadWriter.OnReadCompleted += delegate
-        {
-            var saveSlot = SaveAndLoad.SAVE_SLOT;
-            foreach (var saveData in _moddedSaveData.Values.Where(save => !save.LoadOnStart))
-            {
-                saveData.Load(saveSlot);
-            }
-        };
-
-        __instance._saveFileReadWriter.OnWriteCompleted += delegate
-        {
-            foreach (var saveData in _moddedSaveData.Values)
-            {
-                saveData.Save();
-            }
-        };
     }
 }
