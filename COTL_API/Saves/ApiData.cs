4030acbb17f01d56bf79d13daebbfa47a14a3697
xhayper <hayper1919@gmail.com>
2022-11-12 21:48:51 +0700

    chore: code smell

--- COTL_API/Saves/ApiData.cs
+++ COTL_API/Saves/ApiData.cs
@@ -1,14 +1,11 @@
-﻿using System.Collections.Generic;
-using COTL_API.CustomObjectives;
-
 namespace COTL_API.Saves;
 
-public class APISlotData
+internal class ApiData
 {
     private string[] RandomEasteregg =
         { "Rawr~", "Hello world!", "Another day another unstable API! Hello!", "Proud bug maker!" };
 
     public string OwO => RandomEasteregg[UnityEngine.Random.Range(0, RandomEasteregg.Length)];
 
-    public Dictionary<int, CustomObjective> QuestData { get; } = new();
+    public ObjectDictionary EnumData { get; } = new();
 }
--- COTL_API/Saves/ApiSlotData.cs
+++ COTL_API/Saves/ApiSlotData.cs
@@ -1,11 +1,14 @@
+﻿using System.Collections.Generic;
+using COTL_API.CustomObjectives;
+
 namespace COTL_API.Saves;
 
-internal class APIData
+public class ApiSlotData
 {
     private string[] RandomEasteregg =
         { "Rawr~", "Hello world!", "Another day another unstable API! Hello!", "Proud bug maker!" };
 
     public string OwO => RandomEasteregg[UnityEngine.Random.Range(0, RandomEasteregg.Length)];
 
-    public ObjectDictionary EnumData { get; } = new();
+    public Dictionary<int, CustomObjective> QuestData { get; } = new();
 }
