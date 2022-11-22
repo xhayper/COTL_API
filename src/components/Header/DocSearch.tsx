34199226082788cc73a586d4446d2cfe398d3c91
xhayper <hayper1919@gmail.com>
2022-11-22 14:32:29 +0700

    fix: docsearch yet again

--- src/components/Header/DocSearch.tsx
+++
@@ -1,6 +0,0 @@
-import Search from "./Search";
-
-export default function DocSearch() {
-    // TODO: remove this when docsearch support ESM properly or support preact properly
-    return <Search />;
-}
