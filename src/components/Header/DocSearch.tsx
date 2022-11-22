7e6d02364d938cf92bb814a85a1b1f1479411a33
xhayper <46065349+xhayper@users.noreply.github.com>
2022-11-22 14:30:01 +0700

    feat: fix docsearch

---
+++ src/components/Header/DocSearch.tsx
@@ -0,0 +1,6 @@
+import Search from "./Search";
+
+export default function DocSearch() {
+    // TODO: remove this when docsearch support ESM properly or support preact properly
+    return <Search />;
+}
