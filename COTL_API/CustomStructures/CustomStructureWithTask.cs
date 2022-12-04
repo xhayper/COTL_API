f0d89a5bbc71259a393742e7d0815cfdfe313acb
InfernoDragon0 <mushroomapplepiele@gmail.com>
2022-12-04 15:26:25 +0800

    ci test 5

--- COTL_API/CustomStructures/CustomStructureWithTask.cs
+++ COTL_API/CustomStructures/CustomStructureWithTask.cs
@@ -3,6 +3,7 @@ namespace COTL_API.CustomStructures;
 public abstract class CustomStructureWithTask : CustomStructure, ITaskProvider
 {
     //hewwo?
+    
     public override string InternalName => "Custom_Task_Structure";
     public abstract FollowerTask StructureTask { get; }
 
