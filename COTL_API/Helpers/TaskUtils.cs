888d60917154df6d794abba6d3729f1a45c04f79
Ingo Heijnens <ingohhacks@gmail.com>
2022-12-20 15:52:32 +0100

    TaskUtils structure fix

--- COTL_API/Helpers/TaskUtils.cs
+++ COTL_API/Helpers/TaskUtils.cs
@@ -7,7 +7,7 @@ public static class TaskUtils
     public static Structure? GetAvailableStructureOfType<T>()
     {
         return Structure.Structures.FirstOrDefault(str =>
-            str.Structure_Info.Type == CustomStructureManager.GetStructureByType<T>() &&
+            str.Structure_Info?.Type == CustomStructureManager.GetStructureByType<T>() &&
             str.Brain is { ReservedForTask: false });
     }
 }
