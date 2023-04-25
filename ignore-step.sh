09e0bbcf7bab4500b9c514633ded7520362fe246
xhayper <46065349+xhayper@users.noreply.github.com>
2023-04-25 11:30:02 +0700

    feat: update ignore step

--- ignore-step.sh
+++ ignore-step.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-allowedChanges=("astro.config.ts" "package.json" "yarn.lock" "src/" "public/" ".yarnrc.yml" ".yarn/" "tsconfig.json" "ignore-step.sh")
+allowedChanges=("astro.config.ts" "package.json" "pnpm-lock.yml" "src/" "public/" "tsconfig.json" "ignore-step.sh")
 
 for i in "${allowedChanges[@]}"; do
   flag=$(git diff HEAD^ HEAD "$i")
