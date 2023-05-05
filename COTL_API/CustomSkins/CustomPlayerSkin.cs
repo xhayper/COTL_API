653e801b7805dc48195e5839d2275ca96800ec27
Ingo Heijnens <ingohhacks@gmail.com>
2023-05-05 17:30:15 +0200

    Add simple skin names to player skins

--- COTL_API/CustomSkins/CustomPlayerSkin.cs
+++ COTL_API/CustomSkins/CustomPlayerSkin.cs
@@ -27,11 +27,17 @@ public abstract class CustomPlayerSkin : CustomSkin
                             add = "#" + split[1];
                             simpleName = split[0];
                         }
-
-                        if (from.Attachments.All(x => x.Name != simpleName)) return new List<Tuple<int, string>>();
-
+                        
+                        if (CustomSkinManager.PlayerSkinDict.TryGetValue(simpleName, out var simplified))
+                        {
+                            region.name = simplified.Item1 + ":" + simplified.Item2 + add;
+                            return new List<Tuple<int, string>> {simplified};
+                        }
+                        
                         var atts = from.Attachments.Where(x => x.Name == simpleName);
                         List<Tuple<int, string>> tuples = new();
+                        if (from.Attachments.All(x => x.Name != simpleName)) return new List<Tuple<int, string>>();
+
                         foreach (var att in atts)
                         {
                             region.name = att.SlotIndex + ":" + att.Name + add;
