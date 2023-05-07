b51d2e22a21ead9fd02c9c20bcc3d082ad4e9b81
p1xel8ted <10510767+p1xel8ted@users.noreply.github.com>
2023-05-07 16:51:07 +1000

    Correct DataManager and remove rogue custom structures and mating tents.

--- COTL_API/UI/Helpers/Fixes.cs
+++ COTL_API/UI/Helpers/Fixes.cs
@@ -1,4 +1,7 @@
-﻿using HarmonyLib;
+﻿using System.Diagnostics;
+using System.Linq;
+using COTL_API.CustomStructures;
+using HarmonyLib;
 using MMRoomGeneration;
 using UnityEngine;
 using Object = UnityEngine.Object;
@@ -13,68 +16,110 @@ namespace COTL_API.UI.Helpers
         [HarmonyPatch(typeof(GenerateRoom), nameof(GenerateRoom.OnDisable))]
         private static void GenerateRoom_OnDisable(ref GenerateRoom __instance)
         {
-            // Check if the tent should be removed
-            if (!ShouldTentBeRemoved()) return;
+            // Check if the MatingTentMod is installed
+            if (!MatingTentModExists())
+            {
+                // Log the removal of mating tents
+                LogWarning("MatingTentMod not found, checking for and removing any rogue tents.");
 
-            // Log the removal of mating tents
-            LogWarning("MatingTentMod not found, removing any rogue tents.");
+                // Remove any rogue mating tent GameObjects
+                RemoveMatingTentGameObjects();
+            }
 
-            // Destroy all found mating tent GameObjects
-            RemoveMatingTentGameObjects();
+            // Log the removal of rogue custom structures and DataManager correction
+            LogWarning("Checking other custom structures and correcting DataManager (SaveData)");
 
-            LogWarning("Correcting DataManager (SaveData)");
-
-            // Iterate through the fields, removing mating tents from their respective lists
-            RemoveMatingTentsFromDataManager();
+            // Remove any rogue custom structures from DataManager
+            RemoveRogueCustomStructuresFromDataManager();
         }
 
+        // Method to remove mating tent GameObjects
         private static void RemoveMatingTentGameObjects()
         {
             // Find all GameObjects containing "Mating" in their names
             var tentObjects = Object.FindObjectsOfType<GameObject>()
                 .Where(obj => obj.name.Contains("Mating")).ToList();
 
-            if (tentObjects.Any())
+            if (!tentObjects.Any()) return;
+
+            // Destroy all found mating tent GameObjects
+            foreach (var tentObject in tentObjects)
             {
-                foreach (var tentObject in tentObjects)
-                {
-                    Object.DestroyImmediate(tentObject);
-                    LogWarning("Destroyed mating tent game object.");
-                }
-            }
-            else
-            {
-                LogWarning("Unable to find any tents!");
+                Object.DestroyImmediate(tentObject);
+                LogWarning("Destroyed mating tent game object.");
             }
         }
 
-        private static void RemoveMatingTentsFromDataManager()
+        // Method to remove rogue custom structures from DataManager
+        private static void RemoveRogueCustomStructuresFromDataManager()
         {
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+
             // Get all fields of type List<StructuresData> in DataManager
             var listOfStructuresDataFields = AccessTools.GetDeclaredFields(typeof(DataManager)).Where(a => a.FieldType == typeof(List<StructuresData>)).ToList();
-
-            // Iterate through the fields, removing mating tents from their respective lists
+            bool dataFixed = false;
+            // Iterate through the fields, removing mating tents and custom structures that do not exist in the CustomStructureList
             foreach (var field in listOfStructuresDataFields)
             {
                 if (field.GetValue(DataManager.Instance) is not List<StructuresData> f) continue;
 
-                // Remove all mating tents from the list and log the count
-                var tentCount = f.RemoveAll(a => a == null || (a is {PrefabPath: not null} && a.PrefabPath.Contains("Mating")));
-                field.SetValue(DataManager.Instance, f);
-                if (tentCount > 0)
+                var itemsToRemove = new List<StructuresData>();
+
+                // Check for custom structures that need to be removed
+                f.ForEach(structure =>
                 {
-                    LogWarning($"Removed {tentCount} orphaned tent(s) from {field.Name}.");
+                    if (structure.PrefabPath != null && structure.PrefabPath.Contains("CustomBuildingPrefab"))
+                    {
+                        LogInfo($"Found custom item in {field.Name}: {structure.PrefabPath}");
+                        if (CustomStructureManager.CustomStructureExists(structure.PrefabPath)) return;
+
+                        // Item needs to be removed from BaseStructures
+                        LogWarning($"Found custom item in {field.Name} from a mod no longer installed!: {structure.PrefabPath}");
+                        itemsToRemove.Add(structure);
+                    }
+                });
+
+                // Remove custom structures
+                var customCount = f.RemoveAll(a => itemsToRemove.Contains(a));
+
+                // Remove mating tents
+                var tentCount = 0;
+                if (!MatingTentModExists())
+                {
+                    tentCount = f.RemoveAll(a => a == null || (a is {PrefabPath: not null} && a.PrefabPath.Contains("Mating")));
                 }
+
+                // Update the field in DataManager with the modified list
+                field.SetValue(DataManager.Instance, f);
+
+                // Log the number of removed custom structures and mating tents
+                if (customCount > 0 || tentCount > 0)
+                {
+                    dataFixed = true;
+                    LogInfo($"Removed {customCount} orphaned structure(s) and {tentCount} orphaned tent(s) from {field.Name}.");
+                }
+            }
+
+            // Save the changes and log the time taken for the process
+            stopWatch.Stop();
+            if (dataFixed)
+            {
+                LogInfo($"Finished correcting DataManager (SaveData) in {stopWatch.ElapsedMilliseconds}ms & {stopWatch.ElapsedTicks} ticks.");
+                SaveAndLoad.Save();
+            }
+            else
+            {
+                LogInfo($"No orphaned structure(s), so no changes made to DataManager (SaveData) in {stopWatch.ElapsedMilliseconds}ms & {stopWatch.ElapsedTicks} ticks.");
             }
         }
 
-
-        // Helper method to determine if the tent should be removed
-        private static bool ShouldTentBeRemoved()
+        // Method to check if the MatingTentMod is installed
+        private static bool MatingTentModExists()
         {
-            // Check if the MatingTentMod is not present
+            // Find the MatingTentMod plugin in the BepInEx plugin list
             var matingTentMod = BepInEx.Bootstrap.Chainloader.PluginInfos.FirstOrDefault(a => a.Value.Metadata.GUID.Contains("MatingTentMod")).Value;
-            return matingTentMod == null;
+            return matingTentMod != null;
         }
     }
 }
