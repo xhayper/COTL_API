8d2048195b19b699b23c43b32cd543634539b4d9
xhayper <hayper1919@gmail.com>
2023-05-22 14:42:08 +0700

    feat: change how localization work

---
+++ COTL_API/CustomLocalization/CustomLocalizationManager.cs
@@ -0,0 +1,55 @@
+﻿using HarmonyLib;
+
+namespace COTL_API.CustomLocalization;
+
+[HarmonyPatch]
+public partial class CustomLocalizationManager
+{
+    public static Dictionary<string, Dictionary<string, string>> LocalizationMap { get; } = new();
+    public static List<string> LanguageList { get; } = new();
+
+    /// <summary>
+    /// Loads a localization file from a path.
+    /// </summary>
+    /// <param name="name">The name of the language.</param>
+    /// <param name="path">The path to the localization file.</param>
+    public static void LoadLocalization(string name, string path)
+    {
+        if (!LocalizationMap.ContainsKey(name))
+            LocalizationMap.Add(name, new Dictionary<string, string>());
+
+        if (!LanguageList.Contains(name))
+            LanguageList.Add(name);
+
+        if (File.Exists(path))
+        {
+            var lines = File.ReadAllLines(path);
+            var isInsideQuotes = false;
+            foreach (var line in lines)
+            {
+                var key = "";
+                var value = "";
+                for (var i = 0; i < line.Length; i++)
+                    if (line[i] == '"')
+                    {
+                        isInsideQuotes = !isInsideQuotes;
+                    }
+                    else if (line[i] == ',' && !isInsideQuotes)
+                    {
+                        key = line.Substring(1, i - 2);
+                        value = line.Substring(i + 3, line.Length - i - 4);
+                        break;
+                    }
+
+                LocalizationMap[name].Add(key, value);
+            }
+
+            LogDebug($"Loaded localization: {name}");
+        }
+        else
+        {
+            LogError(
+                $"Localization file not found! Please make sure that the path \"{path}\" contains the localization file.");
+        }
+    }
+}
