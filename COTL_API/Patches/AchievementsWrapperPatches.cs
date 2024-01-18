e99e52f2c9c2d390d8168acb6139ffb3a9ef61ec
xhayper <hayper1919@gmail.com>
2024-01-18 19:09:29 +0700

    feat: improve achievementwrapper patches

--- COTL_API/Patches/AchievementsWrapperPatches.cs
+++ COTL_API/Patches/AchievementsWrapperPatches.cs
@@ -1,4 +1,5 @@
 ﻿using HarmonyLib;
+using Unify;
 
 namespace COTL_API.Patches;
 
@@ -7,16 +8,47 @@ public static class AchievementsWrapperPatches
 {
     [HarmonyPatch(typeof(AchievementsWrapper), nameof(AchievementsWrapper.UnlockAchievement))]
     [HarmonyPrefix]
-    private static bool AchievementsWrapper_UnlockAchievement()
+    private static bool AchievementsWrapper_UnlockAchievement(ref Achievement achievementId)
     {
-        return Plugin.Instance == null || !Plugin.Instance.DontSaveAchievement;
+        var achievementSaved = Plugin.Instance == null || !Plugin.Instance.DontSaveAchievement;
+        if (achievementSaved)
+            return true;
+        
+        if (AchievementsWrapper.unlockedAchievements.Contains(achievementId.id))
+            return false;
+        
+        AchievementsWrapper.unlockedAchievements.Add(achievementId.id);
+        
+        var achievementUnlocked = AchievementsWrapper.OnAchievementUnlocked;
+        achievementUnlocked?.Invoke(achievementId.label);
+        
+        if (achievementId == Achievements.Instance.Lookup("platinum"))
+            return false;
+        
+        AchievementsWrapper.compareAchievements();
+        return false;
     }
     
     [HarmonyPatch(typeof(AchievementsWrapper), nameof(AchievementsWrapper.GetAchievementProgress))]
     [HarmonyPrefix]
-    private static bool AchievementsWrapper_GetAchievementProgress()
+    private static bool AchievementsWrapper_GetAchievementProgress(ref List<AchievementProgress> result)
     {
-        return Plugin.Instance == null || !Plugin.Instance.DontSaveAchievement;
+        if (Plugin.Instance == null || !Plugin.Instance.DontSaveAchievement)
+            return true;
+        
+        // Expect Error
+        if (result == null || result.Count == 0)
+            return false;
+        
+        foreach (var achievementProgress in result.Where(achievementProgress => achievementProgress.progress >= 100 &&
+                     !AchievementsWrapper.unlockedAchievements.Contains(achievementProgress.id)))
+        {
+            AchievementsWrapper.unlockedAchievements.Add(achievementProgress.id);
+            var achievementUnlocked = AchievementsWrapper.OnAchievementUnlocked;
+            achievementUnlocked?.Invoke(achievementProgress.name);
+        }
+
+        return false;
     }
 
     [HarmonyPatch(typeof(AchievementsWrapper), nameof(AchievementsWrapper.UnlockPlatinum))]
