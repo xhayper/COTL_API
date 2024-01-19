2428eb9b3ebb21bf560fd68edf3b707ae70b8e29
xhayper <hayper1919@gmail.com>
2024-01-19 14:38:26 +0700

    chore: code cleanup

--- COTL_API/Patches/AchievementsWrapperPatches.cs
+++ COTL_API/Patches/AchievementsWrapperPatches.cs
@@ -12,33 +12,33 @@ public static class AchievementsWrapperPatches
     {
         if (Plugin.Instance == null || !Plugin.Instance.DontSaveAchievement)
             return true;
-        
+
         if (AchievementsWrapper.unlockedAchievements.Contains(achievementId.id))
             return false;
-        
+
         AchievementsWrapper.unlockedAchievements.Add(achievementId.id);
-        
+
         var achievementUnlocked = AchievementsWrapper.OnAchievementUnlocked;
         achievementUnlocked?.Invoke(achievementId.label);
-        
+
         if (achievementId == Achievements.Instance.Lookup("platinum"))
             return false;
-        
+
         AchievementsWrapper.compareAchievements();
         return false;
     }
-    
+
     [HarmonyPatch(typeof(AchievementsWrapper), nameof(AchievementsWrapper.GetAchievementProgress))]
     [HarmonyPrefix]
     private static bool AchievementsWrapper_GetAchievementProgress(ref List<AchievementProgress> result)
     {
         if (Plugin.Instance == null || !Plugin.Instance.DontSaveAchievement)
             return true;
-        
+
         // Expect Error
         if (result == null || result.Count == 0)
             return false;
-        
+
         foreach (var achievementProgress in result.Where(achievementProgress => achievementProgress.progress >= 100 &&
                      !AchievementsWrapper.unlockedAchievements.Contains(achievementProgress.id)))
         {
