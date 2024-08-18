daacf62b8d0ac53974f86b30261f57d6cdeeacea
xhayper <46065349+xhayper@users.noreply.github.com>
2024-08-18 11:13:17 +0700

    fix: bed placement bug, clone it instead

--- COTL_API/Prefabs/CustomPrefabManager.cs
+++ COTL_API/Prefabs/CustomPrefabManager.cs
@@ -95,7 +95,7 @@ public static class CustomPrefabManager
 
     public static GameObject CreatePlacementObjectFor(CustomStructure structure)
     {
-        var obj = TypeAndPlacementObjects.GetByType(StructureBrain.TYPES.BED).PlacementObject;
+        var obj = UnityEngine.Object.Instantiate(TypeAndPlacementObjects.GetByType(StructureBrain.TYPES.BED).PlacementObject);
 
         var po = obj.GetComponent<PlacementObject>();
         po.ToBuildAsset = structure.PrefabPath;
