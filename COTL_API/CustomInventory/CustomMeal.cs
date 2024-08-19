bd92d9a6a380a7a1b3e8d72b56257efdd832179e
zelzmiy <51797223+zelzmiy@users.noreply.github.com>
2024-08-19 02:55:25 -0500

    feat: CustomMeal Item addition

---
+++ COTL_API/CustomInventory/CustomMeal.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static CookingData;
+
+namespace COTL_API.CustomInventory
+{
+    public abstract class CustomMeal : CustomInventoryItem
+    {
+        /// <summary>
+        /// This Meal's Recepie <br/>
+        /// This is a List of Lists, but the first list should only be of length 1, containing a list of all the items for this recepie
+        /// </summary>
+        public abstract List<List<InventoryItem>> Recipe { get; }
+
+        /// <summary>
+        /// A list of the effects that will occur when eating this meal
+        /// </summary>
+        public abstract MealEffect[] MealEffects { get; }
+
+        public override bool IsFood => true;
+
+        /// <summary>
+        /// "Star" level of this food
+        /// Range: 0-3
+        /// </summary>
+        public abstract int SatiationLevel { get; }
+
+        /// <summary>
+        /// This is the amount that "Hunger Circle" is filled in when cooking the meal.
+        /// Range: 0-1
+        /// </summary>
+        public abstract float TummyRating { get; }
+
+
+    }
+}
---
+++ COTL_API/CustomInventory/Patches/CustomMealPatches.cs
@@ -0,0 +1,76 @@
+using HarmonyLib;
+using Socket.Newtonsoft.Json;
+using src.UI.InfoCards;
+using System;
+
+namespace COTL_API.CustomInventory
+{
+    [HarmonyPatch]
+    public static partial class CustomItemManager
+    {
+        [HarmonyPatch(typeof(CookingData), nameof(CookingData.GetRecipe)), HarmonyPostfix]
+        public static void AddCustomRecepies(ref List<List<InventoryItem>> __result, InventoryItem.ITEM_TYPE mealType)
+        {
+            if (!CustomItemList.ContainsKey(mealType))
+                return;
+
+            __result = (CustomItemList[mealType] as CustomMeal)!.Recipe;
+        }
+
+        [HarmonyPatch(typeof(CookingData), nameof(CookingData.GetMealEffects)), HarmonyPostfix]
+        public static void AddCustomMealEffects(ref CookingData.MealEffect[] __result, InventoryItem.ITEM_TYPE mealType)
+        {
+            if (!CustomItemList.ContainsKey(mealType))
+                return;
+
+            __result = (CustomItemList[mealType] as CustomMeal)!.MealEffects;
+        }
+
+
+        [HarmonyPatch(typeof(CookingData), nameof(CookingData.GetTummyRating)), HarmonyPostfix]
+        public static void GetCustomMealTummyRating(ref float __result, InventoryItem.ITEM_TYPE meal)
+        {
+            if (!CustomItemList.ContainsKey(meal))
+                return;
+
+            __result = (CustomItemList[meal] as CustomMeal)!.TummyRating;
+        }
+
+        [HarmonyPatch(typeof(CookingData), nameof(CookingData.GetSatationLevel)), HarmonyPostfix]
+        public static void GetCustomSatiationLevel(ref int __result, InventoryItem.ITEM_TYPE meal)
+        {
+            if (!CustomItemList.ContainsKey(meal))
+                return;
+
+            __result = (CustomItemList[meal] as CustomMeal)!.SatiationLevel;
+        }
+
+        [HarmonyPatch(typeof(CookingData), nameof(CookingData.GetAllMeals)), HarmonyPostfix]
+        public static void AddCustomMeal(ref InventoryItem.ITEM_TYPE[] __result)
+        {
+            var customMeals = (CustomMeal[])CustomItemList.Values.Where(x => x is CustomMeal).ToArray();
+            var customItems = CustomItemList.Keys.Where(x => CustomItemList[x] is CustomMeal).ToArray(); // ????
+            var newResult = new InventoryItem.ITEM_TYPE[__result.Length + customMeals.Length];
+
+            for (var i = 0; i < __result.Length; i++)
+            {
+                newResult[i] = __result[i];
+            }
+
+            for (var i = 0; i < customMeals.Length; i++)
+            {
+                newResult[__result.Length + i] = customItems[i];
+            }
+
+            __result = newResult;
+        }
+
+        [HarmonyPatch(typeof(RecipeInfoCard), nameof(RecipeInfoCard.Configure)), HarmonyPrefix]
+        public static void AddCustomStarRating(RecipeInfoCard __instance, InventoryItem.ITEM_TYPE config)
+        {
+            var satationLevel = CookingData.GetSatationLevel(config);
+            for (var index = 0; index < __instance._starFills.Length; ++index)
+                __instance._starFills[index].SetActive(satationLevel >= index + 1);
+        }
+    }
+}
