f5385fb7d882bd248169aa45467eb1122449f5b4
xhayper <hayper1919@gmail.com>
2024-08-21 21:51:32 +0700

    feat: update the docs

--- README.md
+++
@@ -1,3 +0,0 @@
-# cotl_api_docs
-
-Documentation for Cult of the Lamb API
--- astro.config.ts
+++ astro.config.ts
@@ -1,3 +1,4 @@
+import starlightLinksValidator from "starlight-links-validator";
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 
@@ -5,41 +6,43 @@ const site = "https://cotl-api.vercel.app/";
 
 // https://astro.build/config
 export default defineConfig({
-    site,
-    integrations: [
-        starlight({
-            title: "COTL_API",
-            logo: {
-                src: "./src/assets/logo.svg"
-            },
-            editLink: {
-                baseUrl: "https://github.com/xhayper/COTL_API/edit/main/docs"
-            },
-            social: {
-                github: "https://github.com/xhayper/COTL_API",
-                discord: "https://discord.gg/jZ2DytX3TX"
-            },
-            head: [
-                {
-                    tag: "meta",
-                    attrs: { property: "og:image", content: `${site}img/banner.png` }
-                },
-                {
-                    tag: "meta",
-                    attrs: { property: "twitter:image", content: `${site}img/banner.png` }
-                }
-            ],
-            sidebar: [
-                {
-                    label: "Getting Started",
-                    autogenerate: { directory: "getting-started" }
-                },
-                {
-                    label: "Reference",
-                    autogenerate: { directory: "reference" }
-                }
-            ],
-            lastUpdated: true
-        })
-    ]
+  site,
+  integrations: [
+    starlight({
+      plugins: [starlightLinksValidator()],
+      title: "COTL_API",
+      logo: {
+        src: "./src/assets/logo.svg"
+      },
+      editLink: {
+        baseUrl: "https://github.com/xhayper/COTL_API/edit/main/docs"
+      },
+      social: {
+        github: "https://github.com/xhayper/COTL_API",
+        discord: "https://discord.gg/jZ2DytX3TX"
+      },
+      head: [
+        {
+          tag: "meta",
+          attrs: { property: "og:image", content: `${site}img/banner.png` }
+        },
+        {
+          tag: "meta",
+          attrs: { property: "twitter:image", content: `${site}img/banner.png` }
+        }
+      ],
+      sidebar: [
+        {
+          label: "Getting Started",
+          autogenerate: { directory: "getting-started" }
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" }
+        }
+      ],
+      lastUpdated: true,
+      credits: true
+    })
+  ]
 });
