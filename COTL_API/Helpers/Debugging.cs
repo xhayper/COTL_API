a870032d134fa76aa638876870f0be3a21166a3a
xhayper <hayper1919@gmail.com>
2024-08-23 10:19:09 +0700

    fix: missing prefab manager

--- COTL_API/Helpers/Debugging.cs
+++ COTL_API/Helpers/Debugging.cs
@@ -5,6 +5,7 @@ using COTL_API.CustomObjectives;
 using COTL_API.CustomRelics;
 using COTL_API.CustomStructures;
 using COTL_API.CustomTasks;
+using COTL_API.Prefabs;
 
 namespace COTL_API.Helpers;
 
