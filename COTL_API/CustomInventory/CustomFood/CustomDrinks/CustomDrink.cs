e07bb963bc4449d7dede164fef577b0983d443d4
zelzmiy <51797223+zelzmiy@users.noreply.github.com>
2025-01-14 02:28:43 -0600

    seal these.

--- COTL_API/CustomInventory/CustomFood/CustomDrinks/CustomDrink.cs
+++ COTL_API/CustomInventory/CustomFood/CustomDrinks/CustomDrink.cs
@@ -3,7 +3,7 @@ namespace COTL_API.CustomInventory;
 public abstract class CustomDrink : CustomFood
 {
     internal FollowerBrain.PleasureActions PleasureAction { get; set; }
-    public override InventoryItem.ITEM_TYPE ItemPickUpToImitate { get; } = InventoryItem.ITEM_TYPE.DRINK_GIN;
+    public sealed override InventoryItem.ITEM_TYPE ItemPickUpToImitate { get; } = InventoryItem.ITEM_TYPE.DRINK_GIN;
 
     /// <summary>
     ///     The amount of Sin gained by the follower. Range: 0-65
